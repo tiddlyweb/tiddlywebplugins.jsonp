c8a763f235b951b8bd596b922f99c3c3d80faae0
Chris Dent <chris.dent@gmail.com>
2011-11-08 12:37:35 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-# YOU NEED TO EDIT THESE
-AUTHOR = 'Python Person'
-AUTHOR_EMAIL = 'python@example.org'
-NAME = 'tiddlywebplugins.example'
-DESCRIPTION = 'The short description of my project'
-VERSION = '0.9'
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.jsonp'
+DESCRIPTION = 'JSONP serialization for TiddlyWeb'
+VERSION = '0.5'
 
 
 import os
@@ -11,7 +10,6 @@ import os
 from setuptools import setup, find_packages
 
 
-# You should carefully review the below (install_requires especially).
 setup(
     namespace_packages = ['tiddlywebplugins'],
     name = NAME,
